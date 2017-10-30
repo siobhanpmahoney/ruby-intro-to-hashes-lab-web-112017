0fd36b94aabccb5f27cbe88f2bd4edaecbc9e3f0
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-30 19:04:05 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 def monopoly_with_second_tier
 	monopoly = {}
 	monopoly[:railroads] = {}
-	monopoly[:railroads] = {pieces: 4, names: {}, rent_in_dollars: {}}
+	monopoly[:railroads] = {:pieces => 4, :names => {}, :rent_in_dollars => {}}
 end
 
 def monopoly_with_third_tier
